a0a7df4a3e0e58b398571b3d87a2b7ee2fa77b35
Claudine <claudine.chen@gmail.com>
2024-09-16 17:24:45 +0200

    create db file from originals

--- scripts/build_database_from_existing_files.py
+++ scripts/build_database_from_existing_files.py
@@ -10,26 +10,33 @@ import re
 import json
 import pathlib
 from ruamel.yaml import YAML
+from collections import defaultdict
 
 
 def read_contribs_text(filepath):
   contribs_list = []
   this_contrib = {}
   contrib_empty = True
-
-  url_pattern = re.compile(r"(.*)=(.*)")
+  contrib_field_counts = defaultdict(int)
 
   with open(filepath, 'r') as f:
     for line in f.readlines():
-      if line.strip() == "" and not contrib_empty:
-        contribs_list.append(this_contrib)
-        this_contrib = {}
-        contrib_empty = True
-      result = url_pattern.findall(line)
-      if result:
-        field, value = result[0]
-        this_contrib[field] = value
+      if line.strip() == "":
+        if not contrib_empty:
+          for key in list(this_contrib.keys()):
+            contrib_field_counts[key] += 1
+          contribs_list.append(this_contrib)
+          this_contrib = {}
+          contrib_empty = True
+
+      str_index = line.find("=")  # capture first equals,
+      if str_index >= 0:
+        field, value = line.split("=", 1)
+        this_contrib[field.strip()] = value.strip()
         contrib_empty = False
+
+  with open("contribs_txt_field_counts.json", 'w') as f:
+    json.dump(contrib_field_counts, f)
 
   return contribs_list
 
@@ -47,8 +54,8 @@ def read_sources_folder(folderpath):
 
 
 if __name__ == "__main__":
-  sources_dir = '../sources'
-  contribs_text_file = '../pde/contribs.txt'
+  sources_dir = '../sources_original'
+  contribs_text_file = '../pde_original/contribs.txt'
   database_file = '../contributions.yaml'
 
   # read in from source files

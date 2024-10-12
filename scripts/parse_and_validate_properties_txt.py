0edfd33098dd6bf645b373f22e2fa4698ee7ae78
Claudine <claudine.chen@gmail.com>
2024-10-12 12:56:23 +0100

    simplify error message in comment

--- scripts/parse_and_validate_properties_txt.py
+++ scripts/parse_and_validate_properties_txt.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
     url = args.url
     if not url.startswith("http"):
         print(f"Url not valid: {url}.\nStopping...")
-        set_output_error(f"Url is not valid. It should start with http(s).")
+        set_output_error(f"Url is not valid. It should start with http or https.")
         raise AssertionError
 
     print(f"url: {url}")  # just for debugging, should do this via logging levels
@@ -143,7 +143,7 @@ if __name__ == "__main__":
     try:
         properties_raw = read_properties_txt(url)
     except Exception as e:
-        set_output_error(f'Error when accessing url: {e}')
+        set_output_error(f'Error when accessing url. Please ensure the url returns a valid properties text file.')
         raise e
 
     print(f"properties text: {properties_raw}")  # just for debugging, should do this via logging levels
@@ -151,7 +151,7 @@ if __name__ == "__main__":
     try:
         props = parse_and_validate_text(properties_raw)
     except Exception as e:
-        set_output_error(f'Error(s) when parsing file: {e}')
+        set_output_error(f'Errors when parsing file. Please check all required fields, and file format.\n\n{e}')
         raise e
 
     props["props"] = url
